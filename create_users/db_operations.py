bb2875932ca1d8112edf650131805ec4ea525f00
trainingdata_candidates <kucev@candidates.ru-central1.internal>
2022-09-27 11:19:18 +0300

    added comments to

--- create_users/db_operations.py
+++ create_users/db_operations.py
@@ -6,7 +6,35 @@ from .types import T_connection
 
 
 @reconnect_on_failure
-def get_all_candidates(*, conn: T_connection) -> list[tuple] | None:
+def get_all_candidates(*, conn: T_connection) -> list[str | None]:
+    """Returns normalised list of all candidates from database.
+
+    Parameters
+    ----------
+    conn : psycopg2.connection
+        Connection object with info about connection to db.
+        Must be passed as a keyword-argument.
+
+    Returns
+    -------
+    candidates : list[str | None]
+        List of candidates (telegram usernames) from db.
+        If no candidates present, returns `[]`.
+    
+    Raises
+    ------
+    OperationalError, InternalError, InterfaceError
+        In case connection to database goes down, one of these
+        is raised.
+
+    Notes
+    -----
+    Catches disconnection errors and tries to reconnect.
+
+    See also
+    --------
+    `reconnect_on_failure()`
+    """
     with conn.cursor() as cur:
         cur.execute(f"SELECT * FROM {TABLE_CANDIDATES};")
         result = cur.fetchall()
@@ -16,8 +44,46 @@ def get_all_candidates(*, conn: T_connection) -> list[tuple] | None:
 def add_candidate_to_db(telegram_username: str,
                         candidate_username: str,
                         candidate_password: str,
-                        *, conn: T_connection) -> str | None:
+                        *, conn: T_connection) -> str:
+    """INSERTs candidate's data into database.
 
+    Parameters
+    ----------
+    telegram_username : str
+        Candidate's telegram username.
+    candidate_username : str
+        Candidate's generated username that
+        will be used to create an account on
+        candidates.trainingdata.solutions.
+    candidate_password : str
+        Candidate's generated password that
+        will be used to create an account on
+        candidates.trainingdata.solutions.
+        It is stored UNENCRYPTED.
+    conn : psycopg2.connection
+        Connection object with info about connection to db.
+        Must be passed as a keyword-argument.
+    
+    Returns
+    -------
+    telegram_username : str
+        Telegram username of an added to db candidate.
+
+    Raises
+    ------
+    OperationalError, InternalError, InterfaceError
+        In case connection to database goes down, one of these
+        is raised.
+
+    Notes
+    -----
+    Executes DML INSERT query.
+    Catches disconnection errors and tries to reconnect.
+
+    See also
+    --------
+    `reconnect_on_failure()`
+    """
     with conn.cursor() as cur:
         cur.execute(f"INSERT INTO {TABLE_CANDIDATES}\
                         VALUES (\
@@ -29,19 +95,79 @@ def add_candidate_to_db(telegram_username: str,
         return telegram_username
 
 @reconnect_on_failure
-def delete_candidate_from_db(candidate_username: str,
-                             *, conn: T_connection) -> str | None:
-    username = candidate_username
+def delete_candidate_from_db(telegram_username: str,
+                             *, conn: T_connection) -> str:
+    """DELETEs candidate's data from database.
+
+    Parameters
+    ----------
+    telegram_username : str
+        Candidate's telegram username.
+    conn : psycopg2.connection
+        Connection object with info about connection to db.
+        Must be passed as a keyword-argument.
+    
+    Returns
+    -------
+    telegram_username : str
+        Telegram username of a deleted from db candidate.
+
+    Raises
+    ------
+    OperationalError, InternalError, InterfaceError
+        In case connection to database goes down, one of these
+        is raised.
+
+    Notes
+    -----
+    Executes DML DELETE query.
+    Catches disconnection errors and tries to reconnect.
+
+    See also
+    --------
+    `reconnect_on_failure()`
+    """
+    username = telegram_username
     with conn.cursor() as cur:
         cur.execute(f"DELETE FROM {TABLE_CANDIDATES}\
                         WHERE {username=};")
         conn.commit()
-        return candidate_username
+        return telegram_username
 
 @reconnect_on_failure
-def remember_username_password(candidate_username: str,
+def remember_username_password(telegram_username: str,
                                *, conn: T_connection) -> dict[str, str]:
-    username = candidate_username
+    """Fetches username and password of a candidate.
+
+    Parameters
+    ----------
+    telegram_username : str
+        Candidate's telegram username.
+    conn : psycopg2.connection
+        Connection object with info about connection to db.
+        Must be passed as a keyword-argument.
+    
+    Returns
+    -------
+    account_data : dict[str, str]
+        Dictionary of candidate's account data, e.g.
+        {"username": "SXRu1_test_Annotator", "wsDBjui87"}
+
+    Raises
+    ------
+    OperationalError, InternalError, InterfaceError
+        In case connection to database goes down, one of these
+        is raised.
+
+    Notes
+    -----
+    Catches disconnection errors and tries to reconnect.
+
+    See also
+    --------
+    `reconnect_on_failure()`
+    """
+    username = telegram_username
     with conn.cursor() as cur:
         cur.execute(f"SELECT * FROM {TABLE_CANDIDATES}\
                         WHERE {username=};")
@@ -51,6 +177,13 @@ def remember_username_password(candidate_username: str,
         return result
 
 def error_raised(candidate: str) -> bool:
+    """Redundant function that used to check for `None`.
+
+    It was used when the results of any of the above queries
+    could return `None`. It should be deleted from here and
+    from `to_handlers.py` eventually.
+    TODO
+    """
     if not candidate:
         return True
     else:

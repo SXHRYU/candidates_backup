a8f7ff55af688453b6b754e40c21ef3e00e5c52d
trainingdata_candidates <kucev@candidates.ru-central1.internal>
2022-09-27 14:12:35 +0300

    added comments to getters

--- tasks_generator/getters.py
+++ tasks_generator/getters.py
@@ -2,6 +2,12 @@ from connection import establish_connection, get_user_tasks, get_user
 from .types import T_task, T_user
 
 
+"""`README.md`, sec. "Why Use `getters`" ("Зачем нужны getters").
+
+Helper functions that offer entry-point for `connection` module,
+provide better isolation, higher cohesion and lower coupling.
+"""
+
 def _get_user(username: str) -> T_user:
     session = establish_connection()
     user = get_user(session, username=username)

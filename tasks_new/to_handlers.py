046f0be0b7cefda091fb7633e5c45baf86af8364
trainingdata_candidates <kucev@candidates.ru-central1.internal>
2022-09-27 14:19:10 +0300

    added comments to handlers

--- tasks_new/to_handlers.py
+++ tasks_new/to_handlers.py
@@ -13,7 +13,19 @@ from tasks_generator.getters import (
 from tasks_generator.types import T_images, T_response, T_task, T_user
 
 
+"""Module with driver functions to export to handlers.
+
+Same as `tasks_generator.to_handlers` but with `times_attempted=2`
+in some places.
+"""
+
 def new_task_generated(telegram_username: str) -> bool:
+    """Used to generate new task once it is requested.
+    
+    See also
+    --------
+    `tasks_generator.to_handlers.task_generated()`
+    """
     assignee: T_user = _get_user(telegram_username)
     assignee_id: int = _get_user_id(assignee)
     assignee_username: str = _get_user_username(assignee)

7d81629fcc6666db70af4a13caa740366330f186
trainingdata_candidates <kucev@candidates.ru-central1.internal>
2022-09-28 12:06:43 +0300

    added comments to 'difference.py'

--- tasks_metrics/differences.py
+++ tasks_metrics/differences.py
@@ -3,6 +3,20 @@ import cv2
 
 
 def get_difference(path_to_image1: str, path_to_image2: str) -> float:
+    """Calculates IoU* between 2 images.
+
+    *Intersection over Union:
+    https://medium.com/analytics-vidhya/iou-intersection-over-union-705a39e7acef
+
+    Parameters
+    ----------
+    path_to_image1 : str | pathlib.Path
+    path_to_image2 : str | pathlib.Path
+
+    Returns
+    -------
+    IoU : float
+    """
     img1 = cv2.imread(path_to_image1, 0)
     img2 = cv2.imread(path_to_image2, 0) # Ground Truth.
     

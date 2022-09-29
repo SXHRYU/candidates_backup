0a96020361bf55df49124e8c68ce4823651957d0
Slava Maltsev <slavaxru@gmail.com>
2022-09-29 16:10:17 +0300

    try test

--- tests/create_users_test.py
+++ tests/create_users_test.py
@@ -1,7 +1,9 @@
 import pytest
 import psycopg2
-# from create_users.db_operations import get_all_candidates
+from create_users.db_operations import get_all_candidates
 
+
+TABLE_CANDIDATES = "candidates"
 
 @pytest.fixture
 def db_conn():
@@ -14,15 +16,29 @@ def db_conn():
     )
 
 @pytest.fixture
-def fake_transaction():
-    b = "lolka"
-    yield b
-    print(b)
-    b = "success"
-    print(b)
+def fake_transaction(db_conn):
+    with db_conn.cursor() as cur:
+        cur.execute(f"CREATE TABLE {TABLE_CANDIDATES} \
+                        (username varchar(255), \
+                         candidate_username varchar(255), \
+                         candidate_password varchar(255));")
+        cur.execute(f"INSERT INTO {TABLE_CANDIDATES} \
+                        VALUES ('1', '1_username', '1_password');")
+        cur.execute(f"INSERT INTO {TABLE_CANDIDATES} \
+                        VALUES ('2', '2_username', '2_password');")
+        cur.execute(f"INSERT INTO {TABLE_CANDIDATES} \
+                        VALUES ('3', '3_username', '3_password');")
+        
+        yield
+        cur.execute(f"DELETE FROM {TABLE_CANDIDATES};")
 
 def test_get_all_candidates(db_conn, fake_transaction):
-    print(db_conn)
-    print('something done')
-    assert fake_transaction == "lolka"
+    fake_transaction
+    unwrapped_get_all_candidates = get_all_candidates.__wrapped__
+    result = [
+        ('1', '1_username', '1_password'),
+        ('2', '2_username', '2_password'),
+        ('3', '3_username', '3_password')
+    ]
+    assert unwrapped_get_all_candidates(db_conn) == result
     

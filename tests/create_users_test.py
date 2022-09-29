411cc15fd84106317ec632beb13cb5b67d7ef2da
Slava Maltsev <slavaxru@gmail.com>
2022-09-29 15:12:27 +0300

    preparation for tests and bug fix

--- tests/create_users_test.py
+++ tests/create_users_test.py
@@ -1,2 +1,28 @@
-def test_1():
-    assert(1==2)
+import pytest
+from create_users.db_operations import get_all_candidates
+
+@pytest.fixture
+def fake_db():
+    a = []
+    a.append(1)
+    a.append(2)
+    a.append(3)
+    yield a
+    print(a)
+    a.pop()
+    a.pop()
+    print(a)
+
+@pytest.fixture
+def fake_transaction():
+    b = "lolka"
+    yield b
+    print(b)
+    b = "success"
+    print(b)
+
+def test_get_all_candidates(fake_db, fake_transaction):
+    assert fake_db == [1, 2, 3]
+    print('something done')
+    assert fake_transaction == "lolka"
+    

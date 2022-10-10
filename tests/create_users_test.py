da9746464688ac6acc17608099ffb6eb9a5e347f
Slava Maltsev <slavaxru@gmail.com>
2022-10-10 15:45:29 +0300

    added 'test_add_candidate_to_db'

--- tests/create_users_test.py
+++ tests/create_users_test.py
@@ -24,17 +24,22 @@ def db_conn(monkeypatch):
     DB_USER = getenv("DB_USER")
     DB_PASSWORD = getenv("DB_PASSWORD")
     DB_HOST = getenv("DB_HOST")
-    return psycopg2.connect(
+    conn = psycopg2.connect(
         host=DB_HOST,
         port=DB_PORT,
         dbname=DB_NAME,
         user=DB_USER,
         password=DB_PASSWORD,
     )
+    conn.autocommit = True
+    return conn
 
 @pytest.fixture
-def fake_transaction(db_conn, monkeypatch):
+def mock_table(monkeypatch):
     monkeypatch.setenv("CANDIDATES", "candidates")
+
+@pytest.fixture
+def fake_transaction(db_conn, mock_table):
     TABLE_CANDIDATES = getenv("CANDIDATES")
     with db_conn.cursor() as cur:
         cur.execute(f"CREATE TABLE {TABLE_CANDIDATES} \
@@ -42,20 +47,46 @@ def fake_transaction(db_conn, monkeypatch):
                          candidate_username varchar(255), \
                          candidate_password varchar(255));")
         cur.execute(f"INSERT INTO {TABLE_CANDIDATES} \
-                        VALUES ('1', '1_username', '1_password');")
+                                VALUES ('1', '1_username', '1_password');")
         cur.execute(f"INSERT INTO {TABLE_CANDIDATES} \
                         VALUES ('2', '2_username', '2_password');")
         cur.execute(f"INSERT INTO {TABLE_CANDIDATES} \
                         VALUES ('3', '3_username', '3_password');")
-        db_conn.commit()
         yield
         cur.execute(f"DROP TABLE {TABLE_CANDIDATES};")
-        db_conn.commit()
+
+
+@pytest.fixture
+def fake_select(db_conn, mock_table):
+    TABLE_CANDIDATES = getenv("CANDIDATES")
+    def select():
+        with db_conn.cursor() as cur:
+            cur.execute(f"SELECT * FROM {TABLE_CANDIDATES};")
+            return cur.fetchall()
+    return select
 
 def test_get_all_candidates(db_conn, fake_transaction):
     from create_users.db_operations import get_all_candidates
 
-    fake_transaction
     unwrapped_get_all_candidates = get_all_candidates.__wrapped__
     result = ['1', '2', '3']
     assert unwrapped_get_all_candidates(conn=db_conn) == result
+
+def test_add_candidate_to_db(db_conn, fake_transaction, fake_select):
+    from create_users.db_operations import add_candidate_to_db
+
+    unwrapped_add_candidate_to_db = add_candidate_to_db.__wrapped__
+    unwrapped_add_candidate_to_db(
+        "tester",
+        "tester-username",
+        "tester-password",
+        conn=db_conn
+    )
+
+    results = fake_select()
+    assert results == [
+        ('1', '1_username', '1_password'),
+        ('2', '2_username', '2_password'),
+        ('3', '3_username', '3_password'),
+        ("tester", "tester-username", "tester-password")
+    ]
